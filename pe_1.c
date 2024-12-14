eb722e2c572935bf1136dddb94017c23555c1afb
mskayacioglu <108230934+mskayacioglu@users.noreply.github.com>
2024-12-14 23:19:20 +0300

    Add files via upload

---
+++ pe_1.c
@@ -0,0 +1,17 @@
+//Multiples of 3 or 5
+
+#include <stdio.h>
+#include "pe_fonctions.h"
+
+int main(){
+	int sum = 0;
+	
+	for(int i = 3; i < 1000; i++){
+		if(i % 3 == 0) sum += i;
+		else if(i % 5 == 0) sum += i;		
+	} 
+	
+	printf("%d\n", sum);	
+
+	return 0;
+}
