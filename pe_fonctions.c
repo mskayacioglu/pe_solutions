28563b5d4adc31eeff0afc22d3ec351978e89bea
mskayacioglu <108230934+mskayacioglu@users.noreply.github.com>
2024-12-15 00:09:37 +0300

    Add files via upload

--- pe_fonctions.c
+++ pe_fonctions.c
@@ -5,12 +5,27 @@
 #include <string.h>
 #include "pe_fonctions.h"
 
-bool isPrime(long n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(long num) {
+    if (num <= 1) {
+        return false;
     }
-    return true;
+
+    if (num == 2 || num == 3) {
+        return true;
+    }
+
+    if (num % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true; 
 }
