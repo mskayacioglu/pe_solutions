de6e8368d00d14b83fb64432c5d8fc0b1151dc56
mskayacioglu <108230934+mskayacioglu@users.noreply.github.com>
2024-12-16 07:48:17 +0300

    Add files via upload

--- pe_fonctions.c
+++ pe_fonctions.c
@@ -6,22 +6,14 @@
 #include "pe_fonctions.h"
 
 bool isPrime(long number) {
-    if (number <= 1) {
-        return false;
-    }
+    if (number <= 1) return false;
 
-    if (number == 2 || number == 3) {
-        return true;
-    }
+    if (number == 2 || number == 3) return true;
 
-    if (number % 2 == 0) {
-        return false;
-    }
+    if (number % 2 == 0) return false;
 
     for (int i = 3; i * i <= number; i += 2) {
-        if (number % i == 0) {
-            return false;
-        }
+        if (number % i == 0) return false;
     }
 
     return true; 
