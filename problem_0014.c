661390bf9a61318b206c9b09a49e9df5d934375f
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-06-24 17:41:31 +0300

    Longest Collatz Sequence

--- problem_0014.c
+++ problem_0014.c
@@ -10,28 +10,28 @@
 #include <string.h>
 
 int get_chain_length(long n) {
-    int steps = 0;
-    
-    while (n != 1) {
-        steps++;
-        if (n % 2 == 0)
-            n /= 2;
-        else
-            n = 3 * n + 1;
-    }
-    return steps;
+  int steps = 0;
+
+  while (n != 1) {
+    steps++;
+    if (n % 2 == 0)
+      n /= 2;
+    else
+      n = 3 * n + 1;
+  }
+  return steps;
 }
 
 int main(void) {
-    int max_length = 0, result = 0;
-    
-    for (int i = 2; i < 1000000; i++) {
-        if (get_chain_length(i) > max_length) {
-            max_length = get_chain_length(i);
-            result = i;
-        }
+  int max_length = 0, result = 0;
+
+  for (int i = 2; i < 1000000; i++) {
+    if (get_chain_length(i) > max_length) {
+      max_length = get_chain_length(i);
+      result = i;
     }
-    printf("%d", result);
-    
-    return 0;
+  }
+  printf("%d", result);
+
+  return 0;
 }
