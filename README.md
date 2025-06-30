d173dd280a4bb62d75138816520eb6c7472b1020
mskayacioglu <mskayacioglu@icloud.com>
2025-06-30 13:40:28 +0300

    Power Digit Sum

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 12. [Highly Divisible Triangular Number](https://projecteuler.net/problem=12)  
 13. [Large Sum](https://projecteuler.net/problem=13)
 14. [Longest Collatz Sequence](https://projecteuler.net/problem=14) 
+16. [Power Digit Sum](https://projecteuler.net/problem=16)
---
+++ problem_0016.c
@@ -0,0 +1,42 @@
+// Power Digit Sum
+// Probem 16
+// https://projecteuler.net/problem=16
+
+#include "pe_functions.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_DIGITS 500
+
+int main(void) {
+  int digits[MAX_DIGITS];
+  memset(digits, 0, sizeof(digits));
+  digits[0] = 1;
+  int size = 1;
+
+  for (int i = 0; i < 1000; i++) {
+    int carry = 0;
+    for (int j = 0; j < size; j++) {
+      int product = digits[j] * 2 + carry;
+      digits[j] = product % 10;
+      carry = product / 10;      
+    }
+
+    while (carry) {
+      digits[size] = carry % 10;
+      carry /= 10;
+      size++;
+    }
+  }
+
+  int sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += digits[i];
+  }
+
+  printf("%d\n", sum);
+  return 0;
+}
