d3a69382c6fb07a46ec2a2b52a53a3fc5d33c4bb
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-08-01 19:50:21 +0300

    Number Letter Counts

--- problem_0017.c
+++ problem_0017.c
@@ -10,6 +10,37 @@
 #include <string.h>
 
 int main(void) {
-    printf("%llu\n", lattice_path(20, 20));
+  const char *units[] = {"",     "one", "two",   "three", "four",
+                         "five", "six", "seven", "eight", "nine"};
+  const char *teens[] = {"ten",      "eleven",  "twelve",  "thirteen",
+                         "fourteen", "fifteen", "sixteen", "seventeen",
+                         "eighteen", "nineteen"};
+  const char *tens[] = {"",      "",      "twenty",  "thirty", "forty",
+                        "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+  int counter = 0;
+
+  for (int i = 1; i <= 1000; i++) {
+    if (i == 1000) {
+      counter += 11; // one thousand
+      break;
+    }
+
+    if (i / 100 != 0) {
+      counter += strlen(units[i / 100]);
+      counter += strlen("hundred");
+      if (i % 100 != 0) {
+        counter += strlen("and");
+      }
+    }
+
+    if (i % 100 < 20 && i % 100 >= 10) {
+      counter += strlen(teens[(i % 100) - 10]);
+    } else {
+      counter += strlen(tens[(i % 100) / 10]);
+      counter += strlen(units[i % 10]);
+    }
+  }
+  printf("%d\n", counter);
   return 0;
 }
