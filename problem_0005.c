365ed8a9dc1aadbaaee716977ddb28a9a90b15f7
Mert Samet Kayacıoğlu <mskayacioglu@icloud.com>
2025-10-17 22:14:43 +0300

    Smallest Multiple

--- problem_0005.c
+++ problem_0005.c
@@ -2,15 +2,31 @@
 // Problem 5
 // https://projecteuler.net/problem=5
 
-#include "pe_functions.h"
 #include <stdbool.h>
 #include <stdio.h>
+
+long lcm(long a, long b);
+
+long gcd(long a, long b);
 
 int main(void) {
   long result = 1;
   for (long i = 2; i <= 20; i++) {
     result = lcm(result, i);
   }
-  printf("%ld\n", result);
+  printf("> %ld\n", result);
   return 0;
+}
+
+long lcm(long a, long b) { 
+  return a * b / gcd(a, b); 
+}
+
+long gcd(long a, long b) {
+  while (b != 0) {
+    long tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+  return a;
 }
