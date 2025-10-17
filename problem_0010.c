d22c8bc366dc80a3e3b85dab2e363092c34c6e7d
Mert Samet Kayacıoğlu <mskayacioglu@icloud.com>
2025-10-17 22:08:46 +0300

    Summation of Primes

--- problem_0010.c
+++ problem_0010.c
@@ -2,10 +2,11 @@
 // Problem 10
 // https://projecteuler.net/problem=10
 
-#include "pe_functions.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+
+bool is_prime(long number);
 
 int main(void) {
   long sum = 0;
@@ -14,6 +15,24 @@ int main(void) {
       sum += i;
     }
   }
-  printf("%ld", sum);
+  printf("> %ld\n", sum);
   return 0;
+}
+
+bool is_prime(long number) {
+  if (number <= 1)
+    return false;
+
+  if (number == 2 || number == 3)
+    return true;
+
+  if (number % 2 == 0)
+    return false;
+
+  for (int i = 3; i * i <= number; i += 2) {
+    if (number % i == 0)
+      return false;
+  }
+
+  return true;
 }
