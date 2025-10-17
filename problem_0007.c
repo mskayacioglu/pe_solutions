19420b9772aa0fae2fff288e1a0c4105d1099a84
Mert Samet Kayacıoğlu <mskayacioglu@icloud.com>
2025-10-17 22:12:16 +0300

    10 001st Prime

--- problem_0007.c
+++ problem_0007.c
@@ -2,9 +2,10 @@
 // Problem 7
 // https://projecteuler.net/problem=7
 
-#include "pe_functions.h"
 #include <stdbool.h>
 #include <stdio.h>
+
+bool is_prime(long number);
 
 int main(void) {
   long count = 0, num = 1;
@@ -15,6 +16,24 @@ int main(void) {
       count++;
   }
 
-  printf("%ld", num);
+  printf("> %ld\n", num);
   return 0;
+}
+
+bool is_prime(long number) {
+  if (number <= 1)
+    return false;
+
+  if (number == 2 || number == 3)
+    return true;
+
+  if (number % 2 == 0)
+    return false;
+
+  for (int i = 3; i * i <= number; i += 2) {
+    if (number % i == 0)
+      return false;
+  }
+
+  return true;
 }
