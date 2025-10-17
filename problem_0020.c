0147c485eb0f45904a994aa6f4068f5142b1c860
Mert Samet Kayacıoğlu <mskayacioglu@icloud.com>
2025-10-17 21:58:39 +0300

    Factorial Digit Sum

--- problem_0020.c
+++ problem_0020.c
@@ -2,12 +2,59 @@
 // Problem 20
 // https://projecteuler.net/problem=20
 
-#include "pe_functions.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct Node {
+  int data;
+  struct Node *prev;
+  struct Node *next;
+} Node;
+
+Node *createNode(int data);
+
+void push_back(Node **head, Node **tail, int digit);
+
+void insertNumberReversed(Node **head, Node **tail, const char *number);
+
+void multiplyListByInt(Node **head, Node **tail, int multiplier);
+
+int sumOfDigits(Node *head);
+
+int main(void) {
+  Node *head = NULL, *tail = NULL;
+
+  insertNumberReversed(&head, &tail, "3628800");
+
+  for (int i = 11; i <= 100; i++) {
+    multiplyListByInt(&head, &tail, i);
+  }
+
+  printf("%d\n", sumOfDigits(head));
+
+  return 0;
+}
+
+Node *createNode(int data) {
+  Node *new_node = malloc(sizeof(Node));
+  new_node->data = data;
+  new_node->next = NULL;
+  new_node->prev = NULL;
+  return new_node;
+}
+
+void push_back(Node **head, Node **tail, int digit) {
+  Node *n = createNode(digit);
+  n->prev = *tail;
+  if (*tail)
+    (*tail)->next = n;
+  else
+    *head = n;
+  *tail = n;
+}
 
 void insertNumberReversed(Node **head, Node **tail, const char *number) {
   for (int i = strlen(number) - 1; i >= 0; --i) {
@@ -42,18 +89,4 @@ int sumOfDigits(Node *head) {
     sum += curr->data;
   }
   return sum;
-}
-
-int main(void) {
-  Node *head = NULL, *tail = NULL;
-
-  insertNumberReversed(&head, &tail, "3628800");
-
-  for (int i = 11; i <= 100; i++) {
-    multiplyListByInt(&head, &tail, i);
-  }
-
-  printf("%d\n", sumOfDigits(head));
-
-  return 0;
 }
